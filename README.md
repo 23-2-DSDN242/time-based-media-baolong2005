a77e1d403b305b78c7b734c369ff35514d9d6786
baolong2005 <135151094+baolong2005@users.noreply.github.com>
2024-03-07 00:55:58 +1300

    maeda clock

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ### THIS IS YOUR README
 My idea is that the clock will be a cycle of the sun, moon and stars 
 
+I change my idea into a scenary which the star will fly from the right to the left and for every star appear is every minute
+The moon will be the same as the star
+I intended to make a day scenary which the sun will replace the moon and the clouds will replace the stars
--- maeda_clock.js
+++ maeda_clock.js
@@ -1,9 +1,17 @@
 // Update this function to draw you own maeda clock on a 960x500 canvas
 function draw_clock(obj) {
   // YOUR MAIN CLOCK CODE GOES HERE
-  background(50); //  beige
-  fill(200); // dark grey
-  textSize(40);
+let hr = hour();
+let mn = minute();
+
+  background(0); //  dark
+  fill (200);
+  
+  textSize(200);
   textAlign(CENTER, CENTER);
-  text("YOUR MAEDA CLOCK CODE GOES HERE", width/2, height/2);
+  translate(width/2, height/2);
+  text(hr, 0 - 50, 0);
+  
+  textSize(100)
+  text(mn, 0 + 60, 0);
 }

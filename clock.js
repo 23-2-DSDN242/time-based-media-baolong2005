5a610a9f1f2b3dd97cebfade6d40c4f8fbfc112f
baolong2005 <135151094+baolong2005@users.noreply.github.com>
2024-03-11 12:53:10 +1300

    clock

--- clock.js
+++ clock.js
@@ -1,6 +1,7 @@
 /*
  * use p5.js to draw a clock on a 960x500 canvas
  */
+//loadImage(Coconut_Tree.png);
 function draw_clock(obj) {
   // draw your own clock here based on the values of obj:
   //    obj.hours goes from 0-23
@@ -11,18 +12,26 @@ function draw_clock(obj) {
   //        < 0 if no alarm is set
   //        = 0 if the alarm is currently going off
   //        > 0 --> the number of seconds until alarm should go off
-  background(50); //  beige
+  
+  background(50); //  dark grey
+
   fill(200); // dark grey
-  textSize(40);
-  textAlign(CENTER, CENTER);
-  text("YOUR MAIN CLOCK CODE GOES HERE", width / 2, 200);
+  // Night clock scene with moon and stars
+  ellipse (480, 600, 1200, 500);
+  fill (255);
+  circle (960, 0, 200);
+//  image (Coconut_Tree, 0 , 0);
+
+  // textSize(40);
+  // textAlign(CENTER, CENTER);
+  // text("YOUR MAIN CLOCK CODE GOES HERE", width / 2, 200);
 
 
-  fill(249, 140, 255);// pink
-  ellipse(width / 3, 350, 150);
-  fill(140, 255, 251) // blue
-  ellipse(width / 2, 350, 150);
-  fill(175, 133, 255); // purple
-  ellipse(width / 3 * 2, 350, 150);
+  // fill(249, 140, 255);// pink
+  // ellipse(width / 3, 350, 150);
+  // fill(140, 255, 251) // blue
+  // ellipse(width / 2, 350, 150);
+  // fill(175, 133, 255); // purple
+  // ellipse(width / 3 * 2, 350, 150);
 
 }
--- maeda_clock.js
+++ maeda_clock.js
@@ -1,17 +1,30 @@
 // Update this function to draw you own maeda clock on a 960x500 canvas
+
 function draw_clock(obj) {
+  
   // YOUR MAIN CLOCK CODE GOES HERE
 let hr = hour();
 let mn = minute();
-
+let sc = second();
+let hr_sizeup = map(sc, 0, 59, 100, 200);
+let mn_sizeup = map(sc, 0, 59, 100, 200);
+let hr_sizedown = map(sc, 0, 59, 200, 100);
+let mn_sizedown = map(sc, 0, 59, 200, 100);
+let hr_dis = map(sc, 0, 59, 60, 80);
+let mn_dis = map(sc, 0, 59, 60, 80);
   background(0); //  dark
   fill (200);
-  
-  textSize(200);
+if(hr_sizeup <= 100){
+  textSize(hr_sizeup);} else { textSize(hr_sizedown)}
   textAlign(CENTER, CENTER);
   translate(width/2, height/2);
-  text(hr, 0 - 50, 0);
+  text(hr, 0 - hr_dis, 0);
+  if(mn_sizeup <= 100){
+    textSize(mn_sizedown);} else { textSize(mn_sizeup)}
   
-  textSize(100)
-  text(mn, 0 + 60, 0);
+  text(mn, 0 + mn_dis, 0);
+
+
+  
+  
 }
